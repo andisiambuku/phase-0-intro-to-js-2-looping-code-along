5b2f4d1fd44006991df518971f4fe79761ccaa15
andisiambuku <roselandambuku@gmail.com>
2022-08-10 16:43:09 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+
+function writeCards(name,event) {
+  const thanks_texts = []
+  for (let i = 0; i < name.length; i++) {
+    thanks_texts[i] = `Thank you, ${name[i]}, for the wonderful ${event} gift!`;
+  }
+  return thanks_texts;
+}
+
+let n = 10;
+function countDown(n){
+    while (n >= 0){
+        console.log(n --);
+    }
+    return (countDown);
+}
